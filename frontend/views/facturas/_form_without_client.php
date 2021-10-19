b190cffbb1c32ed6e9bd72ef44eafa5a88470d58
Micol <micolpa08@gmail.com>
2021-10-19 19:52:09 -0400

    Update _form_without_client.php

--- frontend/views/facturas/_form_without_client.php
+++ frontend/views/facturas/_form_without_client.php
@@ -11,19 +11,22 @@ $model->cotizacion = !$model->cotizacion ? 0 : $model->cotizacion;
 <div class="facturas-form row">
 
 
-        <div class="col-md-6">
+        <div class="col-md-5">
                 <?= $form->field($model, 'cliente_nombre')->textInput(['maxlength' => true, 'required' => 'required']) ?>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-7">
                 <?= $form->field($model, 'date')->textInput(['type' => 'date', 'required' => 'required', 'value' => date('Y-m-d')]) ?>
         </div>
 
 
         <!-- <?//= $form->field($model, 'cliente_nombre')->textInput(['maxlength' => true]) ?> -->
-        <div class="col-md-6">
+        <div class="col-md-5">
                 <?= $form->field($model, 'asunto')->textInput(['maxlength' => true, 'required' => 'required']) ?>
         </div>
-        <div class="col-md-5 mt-4 div_pago_checkbox">
+        <div class="col-md-3">
+                <?= $form->field($model, 'comprobante')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4 mt-4 div_pago_checkbox">
                 <?= $form->field($model, 'pagada')->checkBoxList(['1' =>'Si'])->label('Sello de pago: '); ?>
                 <div class="div_fecha_pago" style="display:<?= $model->pagada ? '' : 'none' ?> ">
                         <?= $form->field($model, 'fecha_pagada')->textInput(['type' => 'date'])->label(false) ?>
